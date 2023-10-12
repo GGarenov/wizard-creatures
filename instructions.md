689d2771c0c0741102d43bf230b7e8f7641ea2fd
Georgiy Garenov <g_garenov@outlook.com>
2023-10-12 19:06:10 +0300

    4. Add images and css in public folder 5. Add html files in views folder 6. Configure handlebars - Install express-handlebars     - configure view engine     - add main layout     - fix hyperlink styles (make the links from main.hbs from ./static/img etc to /img)     - render home page in hbs 7. Convert all html views to handlerbars (hbs)

--- instructions.md
+++ instructions.md
@@ -7,5 +7,14 @@
 - Install packages and setup the server
 
 3. Install Express and nodemon
+    - configure static middleware
     - configure bodyparser (app.use(express.urlencoded({ extended: false }));)
-
+    - configure routes
+4. Add images and css in public folder
+5. Add html files in views folder
+6. Configure handlebars - Install express-handlebars
+    - configure view engine
+    - add main layout
+    - fix hyperlink styles (make the links from main.hbs from ./static/img etc to /img)
+    - render home page in hbs 
+7. Convert all html views to handlerbars (hbs)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 const path = require("path");
-const { PORT } = require("./constants");
 
+const { PORT } = require("./constants");
+const routes = require("./router");
+
+//Init
 const app = express();
 
 //Express Configurations
 app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(express.urlencoded({ extended: false }));
 
+//Handlebars COnfiguration
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+//Routes
 app.get("/", (req, res) => {
-  res.send("Hello home page");
+  // res.send("Hello home page");
+  res.render("home");
 });
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}.`));
