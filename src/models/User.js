73e465beb1bbfff3475debb731c6123b880430c1
Georgiy Garenov <g_garenov@outlook.com>
2023-10-14 19:09:36 +0300

    Password check.

--- src/models/User.js
+++ src/models/User.js
@@ -6,6 +6,12 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+userSchema.virtual("repeatPassword").set(function (value) {
+  console.log({ value });
+  if (value !== this.password) {
+    throw new Error("Password missmatch!");
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
