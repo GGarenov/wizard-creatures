ac0cf88b4d0a1db3d73959f32a93c0e662e8c548
Georgiy Garenov <g_garenov@outlook.com>
2023-10-15 16:02:31 +0300

    19. Error Handling     - add 404 page     - redirect missing route to 404     - add global error handler (optional)     - add error message util 20. Show error notification     - show in the main layout     - pass error to render in login and register pages

--- instructions.md
+++ instructions.md
@@ -63,3 +63,11 @@
 18. Dynamic navigation
     - conditional options in navigation
     - add data to res.locals for hbs template
+19. Error Handling
+    - add 404 page
+    - redirect missing route to 404
+    - add global error handler (optional)
+    - add error message util
+20. Show error notification
+    - show in the main layout
+    - pass error to render in login and register pages
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const userService = require("../services/userService");
+const { extractErrorMsgs } = require("../utils/errorHandler");
 
 router.get("/register", (req, res) => {
   res.render("user/register");
@@ -8,14 +9,19 @@ router.get("/register", (req, res) => {
 router.post("/register", async (req, res) => {
   const { firstName, lastName, email, password, repeatPassword } = req.body;
 
-  await userService.register({
-    firstName,
-    lastName,
-    email,
-    password,
-    repeatPassword,
-  });
-  res.redirect("/users/login");
+  try {
+    await userService.register({
+      firstName,
+      lastName,
+      email,
+      password,
+      repeatPassword,
+    });
+    res.redirect("/users/login");
+  } catch (error) {
+    const errorMessages = extractErrorMsgs(error);
+    res.status(404).render("user/register", { errorMessages });
+  }
 });
 
 router.get("/login", (req, res) => {
@@ -23,14 +29,18 @@ router.get("/login", (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-  console.log(req.body);
   const { email, password } = req.body;
 
-  const token = await userService.login(email, password);
-  console.log({ token });
+  try {
+    const token = await userService.login(email, password);
 
-  res.cookie("token", token, { httpOnly: true });
-  res.redirect("/");
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/");
+  } catch (error) {
+    const errorMessages = extractErrorMsgs(error);
+
+    res.status(404).render("user/login", { errorMessages });
+  }
 });
 
 //TO DO: provide logout endpoint
